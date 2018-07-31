9fb33824b95022c2fb559e5e50787747cbc328e0
David Levy <lennox.davidlevy@gmail.com>
2018-07-31 17:26:04 -0400

    added datetime

--- client/components/Schedule.jsx
+++ client/components/Schedule.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import DateTime from 'react-datetime';
+
+
 
 class Schedule extends React.Component {
-  contstructor(props) {
+  constructor(props) {
     super(props);
+
     this.state = {
-      
-
+      emails: []
     };
-
   }
 
+  render() {
+    return (
+      <div>
+        <DateTime />
+      </div>
+    );
+  }
 }
+
+export default Schedule;
