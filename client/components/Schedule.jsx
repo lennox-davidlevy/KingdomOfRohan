942e42d8353eec94f6bb014fee8f5b3363d60464
David Levy <lennox.davidlevy@gmail.com>
2018-08-01 09:16:44 -0400

    morning commit

--- client/components/Schedule.jsx
+++ client/components/Schedule.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import DateTime from 'react-datetime';
+import axios from 'axios';
+
 
 
 
@@ -8,17 +11,54 @@ class Schedule extends React.Component {
     super(props);
 
     this.state = {
-      emails: []
+      date: '',
+      existingUser: [],
+      email: [],
+      value: ''
     };
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleEmailClick = this.handleEmailClick.bind(this);
+  }
+  handleDateChange(event) {
+    this.setState({
+      date: event._d
+    });
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
   }
 
+  handleEmailClick() {
+    axios.post('/checkEmail', {
+      params: {
+        email: this.state.value
+      }
+    })
+
+  }
+
+
+
+
   render() {
+    console.log(this.state.date);
     return (
       <div>
-        <DateTime />
+        <form>
+          <p>When do you want to watch?</p>
+          <DateTime
+            value={this.state.date}
+            open={false}
+            input={true}
+            onBlur={this.handleDateChange}
+          />
+          <p>Do You Want To Invite Anyone?</p>
+          <input type='email' value={this.state.value}></input><button onClick={}>+</button>
+        </form>
       </div>
     );
   }
 }
 
-export default Schedule;
+
+ReactDOM.render(<Schedule />, document.getElementById('sched'));
